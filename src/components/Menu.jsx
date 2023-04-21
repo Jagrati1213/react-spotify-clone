c89c1b2dffacbd50790001dcdbffbd6c9892b603
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-21 12:50:17 +0530

    Jg: remove title

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 
-function Menu({title,menuObject}) {
+function Menu({menuObject}) {
 
     // add active class in menu
     useEffect(()=>{
@@ -17,16 +17,14 @@ function Menu({title,menuObject}) {
   return (
         // Menu container
         <section className='menuContainer my-4'>
-            <p className='text-[#848484] md:text-base  text-sm font-bold mb-3 uppercase md:text-left text-center'>{title}</p>
-
             <ul className='w-full p-0'>
 
                 {/* Render MenuList */}
                 { menuObject && menuObject.map((menu,ind)=>{
                     return <li className='relative md:my-2 my-6 w-full py-2' key={ind}>
-                            <a href="#" className='text-[#848484] flex items-center text-base md:ml-0 ml-2 hover:text-white'>
-                                <i className='text-2xl md:text-lg md:mr-4'>{menu.icon}</i>
-                                <span className='hidden md:inline'>{menu.name}</span>
+                            <a href="#" className='text-[#848484] flex items-center flex-col justify-center text-sm md:ml-0 ml-2 hover:text-white'>
+                                <i className='text-2xl'>{menu.icon}</i>
+                                <span className='inline mt-1'>{menu.name}</span>
                             </a>
                         </li>
                     })
