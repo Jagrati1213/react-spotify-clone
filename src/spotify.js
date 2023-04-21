6308f6f68bb5460bd4f035117d4c1ce3aa3091ab
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-21 11:56:23 +0530

    Jg: add Axios and api

--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const authEndpoint = 'https://accounts.spotify.com/authorize?';
 const clientID = '6c7bb4c9478241c1bd4683c732dbe9aa';
 const redirectUri = 'http://localhost:3000';
@@ -5,5 +7,13 @@ const scopes = ['user-library-read', 'playlist-read-private'];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
+export const apiClient = axios.create({
+    baseURL: 'https://api.spotify.com/v1/',
+});
 
-//#access_token=BQDR3h1XFulYe8Eys8123kNqdDCPYxvnHDTpClRzHp6Zu3W2QVyXWL-uoYJjPs50PxgJrNFrPxNO0VCpn2pqdBuP22AcJtSh2FlJyJ3xKf_LkfO8e6p1XGOyMmLn99NWGyOdtoB4WFmjRZY1_kEQBZ4WiF508q7MMwNmXtihLr6qHTQANjgI0FCk04keu0SVjMaTMkUhedfVXqUqjnCOGqe9&token_type=Bearer&expires_in=3600
+export const setClientToken = (token) => {
+    apiClient.interceptors.request.use(async function (config) {
+        config.headers.Authorization = 'Bearer ' + token;
+        return config;
+    });
+};
