c6e50f4e1f6955d41d872d4f6777feda7be4e6d1
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-21 12:50:44 +0530

    Jg: add style

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -7,29 +7,27 @@ import { MenuPlayList } from './MenuPlayList';
 import { TrackList } from './TrackList';
 
 function LeftMenu() {
-
+// md:w-72
   return (
-    <section className='leftMenu bg-black h-full min-h-screen w-20 min-w-[64px] md:w-72 py-6 px-4 overflow-hidden'>
+    <section className='leftMenu bg-black h-full min-h-screen w-28 min-w-[64px]  py-6 px-4 overflow-hidden flex justify-between flex-col'>
 
       {/* Logo */}
-       <div className="logo-container text-white flex items-center justify-center md:justify-between">
-          <i className='md:text-3xl text-4xl'><FaSpotify/></i>
-          <h2 className='text-xl md:inline hidden'>Spotify</h2>
-          <i className='text-xl md:inline hidden'><FaEllipsisH/></i>
+       <div className="logo-container text-white flex items-center justify-center">
+          <i className='md:text-5xl text-4xl'><FaSpotify/></i>
        </div>
 
        {/* Search */}
-       <div className="search-box w-full h-10 mt-8 text-[#848484] bg-[#252323d6] overflow-hidden rounded-xl hidden md:flex  items-center justify-center">
+       {/* <div className="search-box w-full h-10 mt-8 text-[#848484] bg-[#252323d6] overflow-hidden rounded-xl hidden md:flex  items-center justify-center">
           <i className='text-[#848484] text-xl p-2'><FaSearch/></i>
            <input type="text" placeholder="search.." className='w-full h-full border-none outline-none px-2 bg-transparent text-white text-sm font-bold'/>
       
-       </div>
+       </div> */}
 
        {/* Menu */}
-       <Menu title={'Menu'} menuObject={MenuList}/>
+       <Menu menuObject={MenuList}/>
 
        {/* PlayList */}
-       <MenuPlayList/>
+       {/* <MenuPlayList/> */}
 
        {/* Song-Details */}
        <TrackList/>
