a5e2ad720c5e588f3993c0a1928fcc9cfedbde67
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-21 15:19:01 +0530

    Jg: add new menulist

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { TbMicrophone2} from 'react-icons/tb';
-import { IoMdContact } from 'react-icons/io'
+import { GoPerson } from 'react-icons/go'
 import {RxTimer} from 'react-icons/rx'
 import {BsMusicNoteList} from 'react-icons/bs'
 import {FaHeart,FaThList} from 'react-icons/fa'
 
 const MenuList = [
-    {id:1,icon:<IoMdContact/>,name:"Profile"},
+    {id:1,icon:<GoPerson/>,name:"Profile"},
     {id:2,icon:<FaThList/>,name:"Feed"},
     {id:3,icon:<TbMicrophone2/>,name:"Top Artist"},
     {id:4,icon:<RxTimer/>,name:"Recent"},
