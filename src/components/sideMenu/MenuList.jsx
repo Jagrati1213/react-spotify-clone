0cd4c6c2293e2a36e53c15eb66b0ce7a07d3d9ba
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-23 17:18:24 +0530

    Jg: add player obj

--- src/components/sideMenu/MenuList.jsx
+++ src/components/sideMenu/MenuList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TbMicrophone2} from 'react-icons/tb';
+import { TbMicrophone2,TbPlayerPlayFilled} from 'react-icons/tb';
 import { GoPerson } from 'react-icons/go'
 import {RxTimer} from 'react-icons/rx'
 import {BsMusicNoteList} from 'react-icons/bs'
@@ -7,10 +7,16 @@ import {FaHeart,FaThList} from 'react-icons/fa'
 
 const MenuList = [
     {id:1,icon:<GoPerson/>,name:"Profile",url:''},
+    
     // {id:2,icon:<FaThList/>,name:"Feed",url:'/react-sopity-clone/feed'},
+
     // {id:3,icon:<TbMicrophone2/>,name:"Top Artist",url:'/react-sopity-clone/artist'},
+
     // {id:4,icon:<RxTimer/>,name:"Recent",url:'/react-sopity-clone/recent'},
+
     // {id:5,icon:<FaHeart/>,name:"Favorites",url:'/react-sopity-clone/favorites'},
     {id:6,icon:<BsMusicNoteList/>,name:"PlayList",url:'/playlist'},
+
+    { id:7,icon:<TbPlayerPlayFilled/>,name:'Player',url:'/player'}
 ]
 export {MenuList};
