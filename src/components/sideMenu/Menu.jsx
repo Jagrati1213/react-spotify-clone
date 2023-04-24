94900e83ea2aaa5c6efd73aa9ad58cb56d511ee7
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-24 12:17:06 +0530

    jg: remove obj, create component, check the location

--- src/components/sideMenu/Menu.jsx
+++ src/components/sideMenu/Menu.jsx
@@ -1,37 +1,26 @@
-import React, {useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { TbMicrophone2} from 'react-icons/tb';
+import { GoPerson } from 'react-icons/go'
+import {RxTimer} from 'react-icons/rx'
+import {BsMusicNoteList} from 'react-icons/bs'
+import {FaPlay,FaMusic} from 'react-icons/fa'
+import { MenuList } from './MenuList';
 
-function Menu({menuObject}) {
+function Menu() {
 
-    // add active class in menu
-    useEffect(()=>{
-      const allList = document.querySelector('.menuContainer ul').querySelectorAll('li');
-
-    function changeMenuActive(){
-        allList.forEach(n=>n.classList.remove('active'));
-        this.classList.add('active');
-    }
-    allList.forEach((n)=>{
-        n.addEventListener('click', changeMenuActive);
-    });
-    allList[0].classList.add('active');
-    },[]);
   return (
         // Menu container
         <section className='menuContainer my-4'>
             <ul className='w-full p-0'>
 
                 {/* Render MenuList */}
-                { menuObject && menuObject.map((menu,ind)=>{
-                    return <li className='relative md:my-2 my-6 w-full py-2' key={ind}>
-                            <Link to={menu.url} className='text-[#848484] flex items-center flex-col justify-center text-sm md:ml-0 ml-2 hover:text-white'>
-                                <i className='text-xl'>{menu.icon}</i>
-                                <span className='inline mt-1'>{menu.name}</span>
-                            </Link>
-                        </li>
-                    })
-                }
-            </ul>
+                <MenuList url='/' title='Profile' icon={<GoPerson/>}/>
+                <MenuList url='/artist' title='Top Artist' icon={<TbMicrophone2/>}/>
+                <MenuList url='/track' title='Top Tracks' icon={<FaMusic/>}/>
+                <MenuList url='/recent' title='Recent' icon={<RxTimer/>}/>
+                <MenuList url='/playlist' title='Playlist' icon={<BsMusicNoteList/>}/>
+                {/* <MenuList url='/player' title='Playlist' icon={<FaPlay/>}/> */}
+           </ul>
         </section>
   )
 }
