4599d42e2784ca51a2b7310f5691f917ebf004a0
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-24 12:18:54 +0530

    Jg: add components and remove object and check out active class

--- src/components/sideMenu/MenuList.jsx
+++ src/components/sideMenu/MenuList.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import { IconContext } from 'react-icons/lib';
+import { Link, useLocation } from 'react-router-dom';
 import { TbMicrophone2,TbPlayerPlayFilled} from 'react-icons/tb';
 import { GoPerson } from 'react-icons/go'
 import {RxTimer} from 'react-icons/rx'
 import {BsMusicNoteList} from 'react-icons/bs'
 import {FaHeart,FaThList} from 'react-icons/fa'
 
-const MenuList = [
-    {id:1,icon:<GoPerson/>,name:"Profile",url:''},
-    
-    // {id:2,icon:<FaThList/>,name:"Feed",url:'/react-sopity-clone/feed'},
+function MenuList({url,icon,title}){
 
-    // {id:3,icon:<TbMicrophone2/>,name:"Top Artist",url:'/react-sopity-clone/artist'},
+    // add active class in menu
+     const location =  useLocation();
+     const isActive = location.pathname == url;
+     const btnClass = isActive ? 'active':'';
 
-    // {id:4,icon:<RxTimer/>,name:"Recent",url:'/react-sopity-clone/recent'},
+    return(
+    <li className={`${btnClass} relative md:my-2 my-6 w-full py-2 cursor-pointer`}>
+        <Link to={url} className={`text-[#848484] flex items-center flex-col justify-center text-[12px] md:ml-0 ml-2 hover:text-white`}>  
 
-    // {id:5,icon:<FaHeart/>,name:"Favorites",url:'/react-sopity-clone/favorites'},
-    {id:6,icon:<BsMusicNoteList/>,name:"PlayList",url:'/playlist'},
-
-    { id:7,icon:<TbPlayerPlayFilled/>,name:'Player',url:'/player'}
-]
+            <IconContext.Provider value={{size:'20px',className:'btn-icon'}}>
+                <i className='text-xl'>{icon}</i>
+                <span className='inline mt-1'>{title}</span>
+            </IconContext.Provider>
+                               
+        </Link>
+    </li>
+    )
+}
 export {MenuList};
