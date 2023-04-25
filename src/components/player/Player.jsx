29bc5f2f48d58c2e1c0dcbf71d70173b1fee3e00
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-25 17:42:35 +0530

    jg: add player component

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -12,25 +12,21 @@ function Player() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(()=>{
-
         if(location.state){
           // get all songs from playlist
           apiClient.get("playlists/"+ location.state?.id+ "/tracks").then((res)=> {
               setTracks(res.data.items);
               setCurrentTracks(res.data.items[0].track);
             });
+
         }
     },[location.state]);
+
   return (
-    <div className='screen_container w-full flex'>
-      <div className="left_player_body mr-2 h-full bg-red-500 w-[80%]">
- 
+      <div className="player_screen h-full w-full max-w-screen flex justify-between flex-col">
+        <Songcard album={currentTrack.album} />
+        <Queue tracks={tracks} setCurrentIndex={setCurrentIndex}/>
       </div>
-      <div className="right_player_body h-full bg-green-500 w-[20%] min-w-[64px] flex justify-between flex-col">
-          <Songcard album={currentTrack.album} />
-          <Queue/>
-      </div>
-    </div>
   )
 }
 
