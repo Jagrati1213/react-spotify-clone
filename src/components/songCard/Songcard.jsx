ff31e2008070d86006b8c9d1600edea938528b5d
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-25 17:43:41 +0530

    jg: add album details

--- src/components/songCard/Songcard.jsx
+++ src/components/songCard/Songcard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { AlbumImage } from './AlbumImage';
 import { AlbumInfo } from './AlbumInfo';
 
-function Songcard({album}) {
+function Songcard({ album }) {
   return (
-    <section className='songCard_body w-full h-3/5 bg-green-300 rounded-3xl rounded-br-none rounded-e-none flex justify-center items-center flex-col'>
-       <AlbumImage url={album?.images[0]?.url}/>
-       <AlbumInfo/>
-    </section>
+    <div className='songCard_body w-full py-8 bg-zinc-900 flex items-center flex-wrap px-8'>
+
+      <AlbumImage url={album?.images[0]?.url}/>
+      <AlbumInfo album={album}/>
+    </div>
   )
 }
 
