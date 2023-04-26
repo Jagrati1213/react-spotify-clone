74cb3dfb79aeda6fc14131a675b4a2580b3ef41c
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-26 10:19:07 +0530

    Jg: add audio component

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -3,6 +3,7 @@ import { Songcard } from '../songCard/Songcard';
 import { Queue } from '../queue/Queue';
 import { apiClient } from '../../spotify';
 import { useLocation } from 'react-router';
+import AudioPlayer from '../audiioPlayer/AudioPlayer';
 
 function Player() {
 
@@ -11,14 +12,13 @@ function Player() {
     const [currentTrack, setCurrentTracks] = useState({});
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    // get all songs from playlist
     useEffect(()=>{
         if(location.state){
-          // get all songs from playlist
           apiClient.get("playlists/"+ location.state?.id+ "/tracks").then((res)=> {
               setTracks(res.data.items);
               setCurrentTracks(res.data.items[0].track);
             });
-
         }
     },[location.state]);
 
@@ -33,6 +33,7 @@ function Player() {
       <div className="player_screen h-full w-full max-w-screen flex justify-between flex-col overflow-y-scroll overflow-x-hidden">
         <Songcard album={currentTrack.album} />
         <Queue tracks={tracks} setCurrentIndex={setCurrentIndex}/>
+        <AudioPlayer currentTrack = {currentTrack} />
       </div>
   )
 }
