092258ccc71db9daf0097ffd353fd5713330d940
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-26 18:49:00 +0530

    jg: send audioLink by props

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -11,6 +11,7 @@ function Player() {
     const [tracks, setTracks] = useState([]);
     const [currentTrack, setCurrentTracks] = useState({});
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [audioName, setAudioName] = useState('');
 
     // get all songs from playlist
     useEffect(()=>{
@@ -18,6 +19,7 @@ function Player() {
           apiClient.get("playlists/"+ location.state?.id+ "/tracks").then((res)=> {
               setTracks(res.data.items);
               setCurrentTracks(res.data.items[0].track);
+              setAudioName(res.data.items[0].preview_url);
             });
         }
     },[location.state]);
@@ -25,6 +27,7 @@ function Player() {
     // Change the player index while click
     useEffect(()=>{
       if(currentIndex){
+        setAudioName(tracks[currentIndex].track.preview_url);
         setCurrentTracks(tracks[currentIndex].track);
       }
     },[currentIndex,tracks]);
@@ -33,7 +36,7 @@ function Player() {
       <div className="player_screen h-full w-full max-w-screen flex justify-between flex-col overflow-y-scroll overflow-x-hidden">
         <Songcard album={currentTrack.album} />
         <Queue tracks={tracks} setCurrentIndex={setCurrentIndex}/>
-        <AudioPlayer currentTrack = {currentTrack} />
+        <AudioPlayer currentTrack = {currentTrack} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} total={tracks} audioName={audioName}/>
       </div>
   )
 }
