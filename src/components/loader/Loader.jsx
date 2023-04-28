fa50bfbb810f943b66ba13debab650c15d369831
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-29 00:01:16 +0530

    jg: font size

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -4,7 +4,7 @@ import '../loader/Loader.css'
 function Loader() {
   return (
     <main className='flex justify-center flex-col items-center h-[100vh]'>
-        <div className="loader w-60 h-12 text-center absolute left-2/4 uppercase font-black text-lg text-white">Loading</div>
+        <div className="loader w-60 h-12 text-center absolute left-2/4 uppercase font-black text-base text-white">Loading</div>
     </main>  
     
 )
