c3e7ee3637e06832b62fa7c4e82dafbb79837f08
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-30 16:39:03 +0530

    jg: set,get function for local

--- src/spotify.js
+++ src/spotify.js
@@ -14,10 +14,38 @@ export const apiClient = axios.create({
     baseURL: 'https://api.spotify.com/v1/',
 });
 
+// TOKENS 
+const expiring_time = 3600 * 1000; // 3600 seconds * 1000 = 1 hour in milliseconds
+
+// set timing
+const setTokenTimestamp = () => window.localStorage.setItem('token_timestamp', Date.now());
+
+// set token
+export const setLocalAccessToken = (token) => {
+    setTokenTimestamp();
+    window.localStorage.setItem('access_token', token);
+};
+
+// set refresh token
+// const setLocalRefreshToken = token => window.localStorage.setItem('refresh_token', token);
+
+// get tokens
+const getTokenTimestamp = () => window.localStorage.getItem('token_timestamp');
+export const getLocalAccessToken = () => window.localStorage.getItem('access_token');
+// const getLocalRefreshToken = () => window.localStorage.getItem('refresh_token');
+
+
 // declared method for api
 export const setClientToken = (token) => {
     apiClient.interceptors.request.use(async function (config) {
         config.headers.Authorization = 'Bearer ' + token;
         return config;
     });
+};
+
+export const logout = () => {
+    window.localStorage.removeItem('token_timestamp');
+    window.localStorage.removeItem('access_token');
+    // window.localStorage.removeItem('refresh_token');
+    window.location.reload();
 };
