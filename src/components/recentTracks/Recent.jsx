c6ec5c7736d227bdfdb476695de3a7d10b0ec2dc
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-30 17:06:34 +0530

    jg: add mt-12

--- src/components/recentTracks/Recent.jsx
+++ src/components/recentTracks/Recent.jsx
@@ -13,7 +13,7 @@ function Recent(){
    },[]);
 
   return (
-    <div className='screen_container lg:w-[90%] md:w-10/12 w-full md:max-w-[100%] h-full min-h-screen md:p-18 p-8 pr-3 mx-auto mb-10 overflow-y-scroll ml-auto md:ml-20'>
+    <div className='screen_container lg:w-[90%] md:w-10/12 w-full md:max-w-[100%] h-full min-h-screen md:p-18 p-8 pr-3 mx-auto mb-10 overflow-y-scroll ml-auto md:ml-20 mt-12'>
       <h1 className='text-white text-2xl font-bold mb-3 lg:mx-auto lg:text-left text-center'>Recently Played Tracks</h1>
 
         {recentTracks.length?
