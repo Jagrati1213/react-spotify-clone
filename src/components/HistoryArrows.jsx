f90978d9f0d0ef73a39c97739b8ccc39af0ecbb5
Jagrati Gupta <123jagratigupta@gmail.com>
2023-04-30 17:13:18 +0530

    jg: add history track buttons

--- src/components/HistoryArrows.jsx
+++ src/components/HistoryArrows.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router';
 import { IconContext } from 'react-icons/lib';
 import { FaArrowLeft,FaArrowRight } from "react-icons/fa";
 
 function HistoryArrows() {
+
+  const navigate = useNavigate();
+
   return (
-    <main className='w-[100%] ml-auto md:ml-20 fixed top-0  bg-[#3432321e] py-4 z-10'>
+    <main className='w-[100%] ml-auto md:ml-20 fixed top-0  bg-[#181818] py-4 z-10'>
          <div className='text-white flex ml-2'>
-            <IconContext.Provider value={{size:'18px'}}>
-                  <i className='mx-2 bg-zinc-800 p-2 rounded-full cursor-pointer'><FaArrowLeft/></i>
-                  <i className='mx-2 bg-zinc-800 p-2 rounded-full cursor-pointer'><FaArrowRight/></i>
+            <IconContext.Provider value={{size:'15px'}}>
+                  <i className='mx-2 bg-zinc-950 p-2 rounded-full cursor-pointer' onClick={() => navigate(-1)}><FaArrowLeft/></i>
+                  <i className='mx-2 bg-zinc-950 p-2 rounded-full cursor-pointer' onClick={() => navigate(1)}><FaArrowRight/></i>
             </IconContext.Provider>
          </div>
          <div></div>
